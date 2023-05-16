78a34f65ec6d8ddede0b349db4ea4fd152d3ee2d
adebiyi422 <adebiyi@usc.edu>
2023-05-15 22:02:15 -0400

    fixed the build after the yaml file went loco, finished linting

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -126,22 +126,20 @@ class TestAccountService(TestCase):
     # ADD YOUR TEST CASES HERE ...
 
     def test_get_account_list(self):
-         """It should List All Accounts"""
-         self._create_accounts(5)
-         resp = self.client.get(BASE_URL)
-         self.assertEqual(resp.status_code, status.HTTP_200_OK)
-         data = resp.get_json()
-         self.assertEqual(len(data), 5)
+        """It should List All Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
 
     def test_get_account(self):
-         """It should Read a Single Account"""
-         account = self._create_accounts(1)[0]
-         resp = self.client.get(
-             f"{BASE_URL}/{account.id}", content_type="application/json"
-         )
-         self.assertEqual(resp.status_code, status.HTTP_200_OK)
-         data = resp.get_json()
-         self.assertEqual(data["name"], account.name)
+        """It should Read a Single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(f"{BASE_URL}/{account.id}", content_type="application/json")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
 
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
@@ -168,6 +166,3 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
-
-
-         
